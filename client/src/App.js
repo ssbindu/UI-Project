9de1c9723a9e27b98d60d3146e80484e37bdfbb2
Shithi Sharma Bindu <ssbindu1998@gmail.com>
2022-07-07 00:10:06 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Nav />}>
-            <Route index element={<About />}></Route>
+          <Route index element={<About />}></Route>
             <Route path="register" element={<Register />}></Route>
             <Route path="login" element={<Login />}></Route>
             <Route path="profile" element={<Profile />}></Route>
